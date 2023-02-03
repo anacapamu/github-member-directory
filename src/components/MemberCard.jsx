383f813deec153c731de0a2a5ad1647386580e1f
Nina <ninagetsbored@gmail.com>
2023-02-03 11:51:54 -0800

    Add wireframe, Delete default CSS, Populate MemberCard with mock data

--- src/components/MemberCard.jsx
+++ src/components/MemberCard.jsx
@@ -1,5 +1,16 @@
+import './MemberCard.css';
+
 const MemberCard = () => {
-    return <div>Avatar Name: Username: Location: Email: # of public repos:</div>;
+    return (
+    <div id="card">
+        <div><img id="avatar" src="https://avatars.githubusercontent.com/u/583231?v=4" alt="avatar"></img></div>
+        <div>
+            <h1><a href="https://github.com/octocat">octocat</a></h1>
+            <p>Name: monalisa octocat</p> 
+            <p>Location: San Francisco</p>
+            <p>Email: octocat@github.com</p>
+            <p>Public repos: 2</p></div>
+    </div>)
 };
 
 export default MemberCard;
