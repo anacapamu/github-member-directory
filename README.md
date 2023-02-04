0f4fe10036e8a5e4e801bbefcc7203d88a027f24
Nina <ninagetsbored@gmail.com>
2023-02-04 11:11:40 -0800

    Pull Members Data from GitHub, Update README - Chained listMembers with fetchMembers to prevent infinite renderings of page - Update README with description, installation steps - Deleted default Vite logo

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# github-member-directory
+# GitHub Member Directory
+
+GitHub Member Directory is a web app that pulls and displays GitHub user information in a member card.
+
+## Installation
+
+To run the web app locally:
+
+1. Clone the repo
+2. Install vite
+    ```bash
+    yarn add vite
+    ```
+3. Run app
+    ```bash
+    yarn dev
+    ```
+
+## Development Choices
+
+### Dependencies
+
+* Vite
+* ESLint
+* Prettier
+
+## Additional Features
+
+If I had more time, I would add:
+
+* Unit tests
--- src/App.jsx
+++ src/App.jsx
@@ -3,164 +3,59 @@ import './App.css';
 import MembersDirectory from './components/MembersDirectory';
 
 function App() {
-    const [usernames, setUsernames] = useState([]);
     const [membersData, setMembersData] = useState([]);
 
-    // useEffect(() => {
-    //     const listMembers = () => {
-    //     fetch("https://api.github.com/users")
-    //       .then((res) => res.json())
-    //       .then(
-    //         (result) => {
-    //             console.log(result);
-    //             const logins = []
-    //             result.map(member => logins.push(member.login))
-    //             setUsernames(logins)
-    //         },
-    //         (error) => {
-    //           console.log(error);
-    //         }
-    //       );
-    //     }
-    //     listMembers();
-    //   }, []);
+    useEffect(() => {
+        const listMembers = async () => {
+            return await fetch('https://api.github.com/users')
+                .then((res) => res.json())
+                .then(
+                    (result) => {
+                        return result.map((member) => {
+                            return member.login;
+                        });
+                    },
+                    (error) => {
+                        console.log(error);
+                    }
+                );
+        };
+        listMembers().then((usernames) => {
+            const fetchMembers = async () => {
+                return Promise.all(
+                    usernames.map((username) => {
+                        return fetchMember(username);
+                    })
+                ).then((membersInfo) => {
+                    console.log(membersInfo)
+                    setMembersData(membersInfo)});
+            };
+            fetchMembers();
+        });
+    }, []);
 
-    // useEffect(() => {
-    //     const getMember = () => {
-    //     fetch("https://api.github.com/users/octocat")
-    //       .then((res) => res.json())
-    //       .then(
-    //         (result) => {
-    //           const memberInfo = {
-    //                 "username": result.login,
-    //                 "name": result.name,
-    //                 "email": result.email,
-    //                 "avatarUrl": result.avatar_url,
-    //                 "profileUrl": result.html_url,
-    //                 "location": result.location,
-    //                 "publicRepos": result.public_repos,
-    //           }
-    //           setMembersData(memberInfo)
-    //         },
-    //         (error) => {
-    //           console.log(error);
-    //         }
-    //       );
-    //     }
-    //     getMember();
-    //   }, []);
-    const mockData = [
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-        {
-            username: 'octocat',
-            name: 'monalisa octocat',
-            location: 'San Francisco',
-            email: 'octocat@github.com',
-            publicRepos: '2',
-            avatarUrl: 'https://avatars.githubusercontent.com/u/583231?v=4',
-            profileUrl: 'https://github.com/octocat',
-        },
-    ];
+    const fetchMember = async (username) => {
+        return await fetch(`https://api.github.com/users/${username}`)
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    return {
+                        username: result.login,
+                        name: result.name,
+                        email: result.email,
+                        avatarUrl: result.avatar_url,
+                        profileUrl: result.html_url,
+                        location: result.location,
+                        publicRepos: result.public_repos,
+                    };
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+    };
 
-    return <MembersDirectory members={mockData} />;
+    return <MembersDirectory members={membersData} />;
 }
 
 export default App;
